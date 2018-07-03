cf2027c065b99e41294b109cad085393d817e184
Diego Lara <dyegolara@gmail.com>
2018-07-02 19:00:03 -0500

    Configurar prettier y agregar react-dom

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
-document.getElementById("app").innerHTML = `
-<h1>Hello Parcel!</h1>
-<div>
-  Look
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>
-  for more info about Parcel.
-</div>
-`;
+import { render } from 'react-dom'
+import React from 'react'
+
+const App = () => <div>Holas</div>
+
+render(App, document.getElementById('app'))
