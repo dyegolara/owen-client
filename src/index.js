8774ae97c74e7d7e4cf65d970eda75d1a6460059
Diego Lara <dyegolara@gmail.com>
2018-07-02 21:39:35 -0500

    Cambiar la ubicación del entry point

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import { render } from 'react-dom'
 import React from 'react'
 
-const App = () => <div>Holas</div>
+class App extends React.Component {
+  render() {
+    return <div>Hola</div>
+  }
+}
 
-render(App, document.getElementById('app'))
+render(<App />, document.getElementById('app'))
