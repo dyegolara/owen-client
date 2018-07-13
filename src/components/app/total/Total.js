750c1fece7ea0a31eacd2a8aa80d52549cafd27a
Diego Lara <dyegolara@gmail.com>
2018-07-13 01:02:45 -0500

    Agregar componente de Total

--- src/components/app/total/Total.js
+++ src/components/app/total/Total.js
@@ -1,6 +1,25 @@
 import React from 'react'
 
-export default props => {
-  console.log(props)
-  return <div>Total</div>
+import AppStorage from 'appStorage'
+
+const getTotalLabel = total => {
+  if (total.amount === 0) return 'Debes'
+  return total.to === AppStorage.getUserId() ? 'Te deben' : 'Debes'
 }
+
+const Total = ({ total }) => (
+  <div className='flex-center'>
+    <div className='box'>
+      <div className='level'>
+        <div className='level-item has-text-centered'>
+          <div>
+            <p className='heading'>{getTotalLabel(total)}</p>
+            <p className='title'>$ {total.amount}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+)
+
+export default Total
