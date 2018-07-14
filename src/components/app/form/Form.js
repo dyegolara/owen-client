848942adb51cb1ea7d23316a52088ca9cee92cbf
Diego Lara <dyegolara@gmail.com>
2018-07-13 22:25:43 -0500

    Segundo mock de Form

--- src/components/app/form/Form.js
+++ src/components/app/form/Form.js
@@ -1,16 +1,32 @@
 import React from 'react'
 import Button from 'shared/button'
 
-export default () => (
-  <div className='columns'>
-    <div className='column'>
-      <Button>Me deben</Button>
-    </div>
-    <div className='column'>
-      <input className='input' type='text' />
-    </div>
-    <div className='column'>
-      <Button>Debo</Button>
-    </div>
-  </div>
-)
+export default class Form extends React.Component {
+  state = {
+    amount: 0
+  }
+  handleAmountChange = e => {
+    const { value } = e.currentTarget
+    this.setState({ amount: value })
+  }
+  render () {
+    return (
+      <div className='columns'>
+        <div className='column is-2 is-offset-3'>
+          <Button className='is-medium is-fullwidth'>Me deben</Button>
+        </div>
+        <div className='column is-2'>
+          <input
+            className='input is-medium'
+            type='number'
+            onChange={this.handleAmountChange}
+            value={this.state.amount}
+          />
+        </div>
+        <div className='column is-2'>
+          <Button className='is-medium is-fullwidth'>Debo</Button>
+        </div>
+      </div>
+    )
+  }
+}
