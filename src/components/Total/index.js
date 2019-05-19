f7ae6f114a220479f484d6358d96d4c3f34cc83d
Diego Lara <dyegolara@gmail.com>
2019-05-19 02:21:15 -0500

    Convertir Total en componente funcional

--- src/components/Total/index.js
+++ src/components/Total/index.js
@@ -1,31 +1,38 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import AppStorage from 'appStorage';
+import useAuth from 'hooks/useAuth';
 
-const getTotalLabel = (total) => {
-  if (total.amount === 0) return 'Debes';
-  return total.to === AppStorage.getUserId() ? 'Te deben' : 'Debes';
-};
+const Total = ({ total }) => {
+  const { getUserInfo } = useAuth();
 
-const Total = ({ total }) => (
-  <div className='section flex-center'>
-    <div className='level'>
-      <div className='level-item has-text-centered'>
-        <div>
-          <p className='heading'>{getTotalLabel(total)}</p>
-          <p className='title is-1'>
-$
-            {total.amount}
-          </p>
+  const getTotalLabel = () => {
+    const { userId } = getUserInfo();
+    if (total.amount === 0) return 'Debes';
+    return total.to === userId ? 'Te deben' : 'Debes';
+  };
+
+  return (
+    <div className='section flex-center'>
+      <div className='level'>
+        <div className='level-item has-text-centered'>
+          <div>
+            <p className='heading'>{getTotalLabel()}</p>
+            <p className='title is-1'>
+              {`$${total.amount}`}
+            </p>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 Total.propTypes = {
-  total: PropTypes.string.isRequired,
+  total: PropTypes.shape({
+    amount: PropTypes.number.isRequired,
+    to: PropTypes.string.isRequired,
+  }).isRequired,
 };
 
 export default Total;
