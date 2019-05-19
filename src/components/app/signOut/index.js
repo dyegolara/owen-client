3107491da83bbb67f519c99987c961f591683582
Diego Lara <dyegolara@gmail.com>
2019-05-18 22:01:28 -0500

    Mover resto de componentes de app a un index

--- src/components/app/signOut/index.js
+++ src/components/app/signOut/index.js
@@ -1,3 +1,13 @@
-import SignOut from './SignOut';
+import React from 'react';
+import { auth } from '_firebase';
+import Button from 'shared/button';
+
+const SignOut = () => (
+  <Button
+    onClick={() => auth.signOut()}
+    icon='logout'
+    ariaLabel='Cerrar Sesión'
+  />
+);
 
 export default SignOut;
--- src/components/app/total/index.js
+++ src/components/app/total/index.js
@@ -1,3 +1,31 @@
-import Total from './Total';
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import AppStorage from 'appStorage';
+
+const getTotalLabel = (total) => {
+  if (total.amount === 0) return 'Debes';
+  return total.to === AppStorage.getUserId() ? 'Te deben' : 'Debes';
+};
+
+const Total = ({ total }) => (
+  <div className='section flex-center'>
+    <div className='level'>
+      <div className='level-item has-text-centered'>
+        <div>
+          <p className='heading'>{getTotalLabel(total)}</p>
+          <p className='title is-1'>
+$
+            {total.amount}
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+Total.propTypes = {
+  total: PropTypes.string.isRequired,
+};
 
 export default Total;
