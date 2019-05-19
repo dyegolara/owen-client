ab4941b736fa83f07d0887180e67751067da1012
Diego Lara <dyegolara@gmail.com>
2019-05-18 22:06:01 -0500

    Convertir Login a componente funcional

--- src/components/login/index.js
+++ src/components/login/index.js
@@ -1,3 +1,31 @@
-import Login from './Login';
+import React, { useState } from 'react';
+import { auth, googleAuthProvider } from '_firebase';
+
+import Button from 'shared/button';
+import styles from './Login.module.scss';
+
+
+const Login = () => {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const signIn = () => {
+    setIsLoading(true);
+
+    auth.signInWithRedirect(googleAuthProvider)
+      .finally(() => setIsLoading(false));
+  };
+
+  return (
+    <div className={`hero is-fullheight is-bold is-link ${styles.login}`}>
+      <Button
+        className={`is-medium ${isLoading ? 'is-loading' : ''}`}
+        onClick={signIn}
+        icon='google'
+      >
+          Iniciar sesión
+      </Button>
+    </div>
+  );
+};
 
 export default Login;
