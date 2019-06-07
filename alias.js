06e3f52dc21053b9d69825084c44747791c87d22
Diego Lara <dyegolara@gmail.com>
2019-06-07 11:39:28 -0500

    Agregar jsconfig y alias para hooks

--- alias.js
+++ alias.js
@@ -5,6 +5,7 @@ module.exports = {
   appStorage: path.resolve(__dirname, 'src/appStorage/'),
   assets: path.resolve(__dirname, 'src/assets/'),
   components: path.resolve(__dirname, 'src/components/'),
+  hooks: path.resolve(__dirname, 'src/hooks/'),
   _firebase: path.resolve(__dirname, 'src/firebase.js'),
   styles: path.resolve(__dirname, 'src/styles/'),
   utils: path.resolve(__dirname, 'src/utils/'),
